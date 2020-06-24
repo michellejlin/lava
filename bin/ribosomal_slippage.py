6a0de9782473f02cba27f7fcb0e5174576413ca8
michellejlin <42459461+michellejlin@users.noreply.github.com>
2020-06-23 20:27:37 -0700

    ribosomal slippage update for coronavirus
    creates 2 files: final_corrected_slippage.csv (for correct values for final data) and visualization_csv (just adds column so genome_protein_plots.py can take both the wrong one for visualization and the right one for tooltips)

--- bin/ribosomal_slippage.py
+++ bin/ribosomal_slippage.py
@@ -12,39 +12,68 @@ import os.path
 import pandas as pd
 import sys
 
-start_num = ""
-# Grabs where ribosomal slippage happens from proteins.csv.
-start_num_list = []
+correction_number = 0
+# -1 for coronavirus, HIV
+slippage_number = -1
+
+# grabs the start of the CDS for the original protein before ribosomal slip
+slippage_cd_start = open("ribosomal_start.txt").read()
+
 for line in open("proteins.csv"):
     if '_ribosomal_slippage' in line:
-        start_num = line.split(',')[1]
-        if not start_num in start_num_list:
-            start_num_list.append(start_num)
+        slip_site = line.split(',')[1]
+        correction_number = int(slip_site) - int(slippage_cd_start) - slippage_number
 
 # Writes corrected lines in new file.
-temp = open('temp.csv', 'w')
-index = 0
-if start_num != "":
-    for line in open("merged.csv", 'r'):
+temp = open('final_corrected_slippage.csv', 'w')
+temp2 = open('visualization.csv', 'w')
+
+residue_correction_number = correction_number / 3
+correction_number = int(correction_number)
+residue_correction_number = int(residue_correction_number)
+
+with open("final.csv") as f:
+    header = f.readline()
+    temp.write(header)
+    temp2.write(header.rstrip() + ",NucCorrect,AminoCorrect" + '\n')
+
+    next(f)
+
+    for row in f:
+        line = row.rstrip()
         # Adds the number of where ribosomal slippage happens to where Annovar begins.
-        if '_ribosomal_slippage' in line:
-            nuc = line.split(',')[6]
-            if 'del' in nuc:
-                nuc_num = int(nuc[0:-4])
-                print("del nuc_num" + str(nuc_num))
-            else:
-                nuc_num = int(nuc[1:-1])
-            amino = line.split(',')[4]
-            amino_num = int(amino[1:-1])
-            position = line.split(',')[2]
-            if position < int(start_num_list[index]):
-                index = index + 1
-            if 'del' in nuc:
-                new_line = line.replace(nuc, str(nuc_num + int(start_num_list[index])) + nuc[-4:])
-            else:
-                new_line = line.replace(nuc, nuc[0] + str(nuc_num + int(start_num_list[index])) + nuc[-1])
-            amino_num2 = int(start_num_list[index])/3
-            new_line = new_line.replace(amino, amino[0] + str(amino_num + amino_num2) + amino[-1])
-            temp.write(new_line)
+        nuc = line.split(',')[6]
+        if 'del' in nuc:
+            nuc_num = int(nuc[0:-4])
+            print("del nuc_num" + str(nuc_num))
         else:
-            temp.write(line)
+            nuc_num = int(nuc[1:-1])
+        amino = line.split(',')[4]
+        amino_num = int(amino[1:-1])
+
+        # position = line.split(',')[2]
+            # if position < int(start_num_list[index]):
+            #     index = index + 1
+
+
+        if '_ribosomal_slippage' in line:
+            if 'del' in nuc:
+                del_nuc_replacement = str(nuc_num + correction_number) + nuc[-4:]
+                new_line = line.replace(nuc, del_nuc_replacement)
+                temp2_line = new_line + "," + del_nuc_replacement
+            else:
+                nuc_replacement = nuc[0] + str(nuc_num + correction_number) + nuc[-1]
+                new_line = line.replace(nuc, nuc_replacement)
+                temp2_line = new_line + "," + nuc_replacement
+
+            amino_replacement = amino[0] + str(amino_num + residue_correction_number) + amino[-1]
+            new_line = new_line.replace(amino, amino_replacement)
+
+            temp2_line = temp2_line.replace(amino, amino_replacement)
+            temp2_line = temp2_line + "," + amino_replacement
+
+            temp.write(new_line + '\n')
+            temp2.write(temp2_line + '\n')
+        else:
+            temp.write(line + '\n')
+            temp2.write(line + "," + nuc + "," + amino + '\n')
